c496d4c62928aa7b954164087877107490dece73
EdisonJwa <i@uv.uy>
2021-03-16 19:03:15 +0900

    Create eCampus-notices.user.js

---
+++ eCampus-notices.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         eCampus 공지 위치 변경
+// @namespace    https://uv.uy/
+// @version      0.1
+// @description  Relocate the notices
+// @author       Edison Jwa
+// @match        https://ecampus.kookmin.ac.kr/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var notice = document.getElementsByClassName('modal-ubboard-notice');
+    for (var j = 0; j < notice.length; j++) {
+        var nt = notice[j].style;
+        var resizeNotice = 500;
+        if (self.innerWidth <= 1200 ){
+            nt.setProperty('width', '300px');
+            nt.setProperty('min-width', 'auto');
+            resizeNotice = 300;
+        }
+        if (self.innerWidth <= 800 ){
+            nt.setProperty('height', '50px');
+            nt.setProperty('min-height', 'auto');
+            var smMax = window.innerWidth - resizeNotice - 20 + 'px';
+            nt.setProperty('left', smMax);
+            var smTop = window.innerHeight - ((notice.length - j) * 50) - 5 + 'px';
+            nt.setProperty('top', smTop);
+        } else {
+            var max = window.innerWidth - resizeNotice - ((j + 2) * 20) + 'px';
+            nt.setProperty('left', max);
+        }
+    }
+})();
